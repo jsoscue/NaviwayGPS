e9ce80345aa02ad96e419f2f88c28df755e5f28d
Juan Sebastian Soscue Portes <104285353+jsoscue@users.noreply.github.com>
2024-11-08 20:42:36 -0500

    Delete gps-tracker directory

--- gps-tracker/Admin/rutas/rutas.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('route-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const start = document.getElementById('start').value;
-    const end = document.getElementById('end').value;
-    window.location.href = `../map.html?start=${start}&end=${end}`;
-});
